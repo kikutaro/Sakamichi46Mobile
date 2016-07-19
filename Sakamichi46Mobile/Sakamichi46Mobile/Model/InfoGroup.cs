d441a3cd8c9aa01435108b914cae128096b5f0a2
Yoichi Kikuta <kikutaro526@yahoo.co.jp>
2016-07-19 23:15:51 +0900

    Add application info page.

---
+++ Sakamichi46Mobile/Sakamichi46Mobile/Model/InfoGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakamichi46Mobile.Model
+{
+    public class InfoGroup : ObservableCollection<InfoItem>
+    {
+        public string Title { get; set; }
+
+        public string ShortName { get; set; }
+
+        public InfoGroup(string title, string shortName)
+        {
+            this.Title = title;
+            this.ShortName = shortName;
+        }
+    }
+}
