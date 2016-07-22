779ddb102adef1cf45b5cc09a35831efcfd2d947
Yoichi Kikuta <kikutaro526@yahoo.co.jp>
2016-07-22 20:00:55 +0900

    Modify Info page, remove ListView.

--- Sakamichi46Mobile/Sakamichi46Mobile/Info.xaml.cs
+++ Sakamichi46Mobile/Sakamichi46Mobile/Info.xaml.cs
@@ -4,29 +4,21 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Sakamichi46Mobile.Model;
+using Plugin.Share;
 using Xamarin.Forms;
 
 namespace Sakamichi46Mobile
 {
     public partial class Info : ContentPage
     {
-        public ObservableCollection<InfoGroup> infoGroups;
-
         public Info()
         {
             InitializeComponent();
 
-            infoGroups = new ObservableCollection<InfoGroup>();
-            InfoGroup setting = new InfoGroup("設定", "設定");
-            InfoGroup other = new InfoGroup("その他", "その他");
-            infoGroups.Add(setting);
-            infoGroups.Add(other);
-
-            other.Add(new InfoItem("ご意見・ご要望"));
-            other.Add(new InfoItem("シェア"));
-
-            infoView.ItemsSource = infoGroups;
+            btnShare.Clicked += (o, e) => 
+            {
+                CrossShare.Current.ShareLink("https://github.com/kikutaro/Sakamichi46Mobile", "Sakamichi46 App");
+            };
         }
     }
 }
