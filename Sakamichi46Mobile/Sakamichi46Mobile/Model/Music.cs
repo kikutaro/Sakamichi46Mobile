bb2ad8e54f8aad38416ffad31f743e0e365587b1
Yoichi Kikuta <kikutaro526@yahoo.co.jp>
2016-08-20 12:21:28 +0900

    Add calling Music API.

---
+++ Sakamichi46Mobile/Sakamichi46Mobile/Model/Music.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakamichi46Mobile.Model
+{
+    public class Music
+    {
+        public string title { get; set; }
+
+        public string releaseVersion { get; set; }
+
+        public string releaseDate { get; set; }
+
+        public string lyricUri { get; set; }
+
+        public string coverPhotoUri { get; set; }
+
+        public string officialMovieUri { get; set; }
+
+        public string type { get; set; }
+
+        public Music(string title)
+        {
+            this.title = title;
+        }
+    }
+}
---
+++ Sakamichi46Mobile/Sakamichi46Mobile/Model/MusicCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakamichi46Mobile.Model
+{
+    public class MusicCategory : List<Music>
+    {
+        public string releaseVersion { get; set; }
+
+        public MusicCategory(string releaseVersion)
+        {
+            this.releaseVersion = releaseVersion;
+            
+        }
+    }
+}
